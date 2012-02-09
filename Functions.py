02def3537709bb24618d24cf004ed94573c7f3f4
Vladimir Reinharz <vreinharz@gmail.com>
2012-02-08 20:00:57 -0500

    randomized added

--- Functions.py
+++ Functions.py
@@ -1,5 +1,23 @@
 """This file contains functions that only call standard libraries
 """
+import random
+
+def weighted_selection(elements, weights=None):
+    """Takes as input two list. The first is the elements and in the same
+    order the second list should contain their weights (default all equal) 
+    in the same order. The output is a random element
+    """
+    if weights:
+        r = random.uniform(0,sum(weights))
+        sub_total = 0
+        for i, w in enumerate(weights):
+            sub_total += w
+            if sub_total > r:
+                return elements[i]
+        return elements[i]
+    else:
+        return random.choice(elements)
+
 def bp_positions(sec_struct):
     """This function takes as input a secondary structure,
     without pseudo-knots, and outputs a list of tuples (a,b)
@@ -17,3 +35,6 @@ def bp_positions(sec_struct):
 
 if __name__ == '__main__':
     print bp_positions('....(((.(....)..))).')
+
+    bob = ['a', 'b', 'c']
+    print weighted_selection(bob, [3,1,1])
