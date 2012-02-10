74dcaf31d3c3b6232cd32fccfeeeb86e932e0dc6
Vladimir Reinharz <vreinharz@gmail.com>
2012-02-10 01:48:44 -0500

    nicer tex

--- LaTeX.py
+++ LaTeX.py
@@ -21,7 +21,11 @@ def standard_header(font_size=12, doc_class='article',
                  '\t\\maketitle']
     return '\n'.join(type + packages + title + start_doc)
 
-def figure_env(subfigures_list, width=0.43):
+def figure_env(subfigures_list, width=0.47):
+    """Given a list of figures path,
+    create a LaTeX environment figure with all the subfigure
+    with width "width", by default 0.47 which is 2 per row.
+    """
     environment = ['\\begin{figure}[h!]\n\t\\centering']
     for fig in subfigures_list:
         environment.append('\t\\subfloat{}\\includegraphics[width=%s\\textwidth]{%s}' % (width, fig))
